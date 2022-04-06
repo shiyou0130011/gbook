5a3ce7e72f58d1c31a7ee94bc8302fed4abe3b7f
Tzuyang Tsai <shiyou0130011+github@googlemail.com>
2022-04-07 07:12:58 +0800

    copy dir func

--- io.go
+++ io.go
@@ -3,7 +3,9 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"os"
 	"path"
+	"path/filepath"
 )
 
 // load all files in folderPath
@@ -32,4 +34,27 @@ func copyFile(sourceFolderPath string, outputFolderPath string, relativeFilePath
 	}
 
 	ioutil.WriteFile(path.Join(outputFolderPath, relativeFilePath), data, 0644)
+}
+
+//Copy the source folder to output folder
+func copyDir(sourceFolder string, outputFolder string) {
+	fileInfos, err := ioutil.ReadDir(sourceFolder)
+	if err != nil {
+		log.Printf("Cannot read folder %s", sourceFolder)
+	}
+	for _, fi := range fileInfos {
+		sourcePath := filepath.Join(sourceFolder, fi.Name())
+		outPath := filepath.Join(outputFolder, fi.Name())
+		if fi.IsDir() {
+			err = os.Mkdir(outPath, 0755)
+			if err != nil {
+				log.Printf("Cannot create folder %s", outPath)
+			} else {
+				copyDir(sourcePath, outPath)
+			}
+
+		} else {
+			copyFile(sourcePath, outputFolder, fi.Name())
+		}
+	}
 }
