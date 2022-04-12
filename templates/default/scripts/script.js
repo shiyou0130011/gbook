c6940e0d4aed6dfe16b0f9ece6b6d204f47fc9e7
Tzuyang Tsai <shiyou0130011+github@googlemail.com>
2022-04-12 22:01:03 +0800

    update style of format

--- templates/default/scripts/script.js
+++ templates/default/scripts/script.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
 		var menuStatus = $(document.body).attr("data-menu")
 		$(document.body).attr("data-menu", menuStatus == "open"? "close": "open")
 	})
+
+	$("#format-change").click(function(){
+		$("#format-menu").toggle()
+	})
+
+	var fontSizeList = ["small", "normal", "large"]
+	$("#format-menu .text-decrease").click(function(){
+		var fontIndex = fontSizeList.indexOf(document.body.dataset.size)
+		document.body.dataset.size = fontSizeList[Math.max(fontIndex - 1, 0)]
+	})
+	
+	$("#format-menu .text-increase").click(function(){
+		var fontIndex = fontSizeList.indexOf(document.body.dataset.size)
+		document.body.dataset.size = fontSizeList[Math.min(fontIndex + 1, fontSizeList.length)]
+	})
 })
