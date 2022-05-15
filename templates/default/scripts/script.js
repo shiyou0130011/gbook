78c2ba99cbcc85438a10fc11e047832c68b64cf0
Tzuyang Tsai <shiyou0130011+github@googlemail.com>
2022-05-15 16:13:26 +0800

    format menu 處理

--- templates/default/scripts/script.js
+++ templates/default/scripts/script.js
@@ -12,17 +12,33 @@ $(document).ready(function(){
 	$("#format-menu .text-decrease").click(function(){
 		var fontIndex = fontSizeList.indexOf(document.body.dataset.size)
 		document.body.dataset.size = fontSizeList[Math.max(fontIndex - 1, 0)]
+		
+		localStorage.fontSize = document.body.dataset.size
 	})
 	
 	$("#format-menu .text-increase").click(function(){
 		var fontIndex = fontSizeList.indexOf(document.body.dataset.size)
 		document.body.dataset.size = fontSizeList[Math.min(fontIndex + 1, fontSizeList.length - 1)]
+		
+		localStorage.fontSize = document.body.dataset.size
 	})
 	$("#font-change-sans-serif").click(function(){
 		document.body.dataset.font = "sans-serif"
+
+		localStorage.font = "sans-serif"
 	})
 	$("#font-change-serif").click(function(){
 		document.body.dataset.font = "serif"
+
+		localStorage.font = "serif"
+	})
+	$(".change-theme").click(function(){
+		var theme = this.dataset.targetTheme
+		$("link.theme-change-element").each(function(){
+			$(this).attr("href", $(this).attr(theme))
+		})
+		
+		localStorage.theme = theme
 	})
 })
 $(document).ready(function(){
